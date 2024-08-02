cf972aace9f65a3729c673ec39e3d37879a323e3
yogesh024 <111590497+yogesh024@users.noreply.github.com>
2024-08-02 10:24:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy:{
-      '/api': 'https://back-again-kedv.onrender.com/',
+      '/api': 'https://back-again-kedv.onrender.com',
      },
       },
      
