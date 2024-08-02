d279b0525fa099afc5f8e490528bcfb81612ce88
yogesh024 <111590497+yogesh024@users.noreply.github.com>
2024-08-02 19:16:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy:{
-      '/api': 'http://localhost:3002/',
+      '/api': 'https://job-backend-v75o.onrender.com',
      },
       },
      
